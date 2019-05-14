ed7d6783683b44f25c625e90da31eb213fd7e8e0
meghan miller <mille265@gmail.com>
2019-05-14 21:30:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 class Person
 
-  def initialize(name)
-    @name = name
-  end
-
   def name
     @name
   end
 
-  def initialize(job)
-    @job = job
+  def name = (name)
+    @name = name
   end
 
   def job
     @job
+  end
+
+  def job = (job)
+    @job = job
   end
 end
